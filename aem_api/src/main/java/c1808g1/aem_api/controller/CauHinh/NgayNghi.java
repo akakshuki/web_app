2f897ca464bbb359948f2d997505ac25cd82cf81
dragosasdzxc <duongnguyen5566@gmail.com>
2020-11-22 23:26:52 +0700

    update lần 2 trong đêm

--- aem_api/src/main/java/c1808g1/aem_api/controller/CauHinh/NgayNghi.java
+++ aem_api/src/main/java/c1808g1/aem_api/controller/CauHinh/NgayNghi.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import c1808g1.Models.CauHinh.HolidayDTO;
+import c1808g1.aem_api.DAO.CauHinh.NgayNghiDAO;
 import c1808g1.aem_api.config.ModelMapperConfig;
 import c1808g1.aem_api.models.CauHinh.HolidayModel;
 import c1808g1.aem_api.service.CauHinh.HolidayService;
@@ -29,17 +30,11 @@ import c1808g1.aem_api.service.CauHinh.HolidayService;
 @RequestMapping("/cauhinh/holidayapi")
 public class NgayNghi {
 	private HolidayService holiSv;
-
-	public NgayNghi() {
-		super();
-
-	}
-
-	@Autowired
+    @Autowired
 	public NgayNghi(HolidayService holiSv) {
 		this.holiSv = holiSv;
 	}
-
+	
 	@RequestMapping(value = "/holi", method = RequestMethod.GET)
 	public ResponseEntity<List<HolidayDTO>> findAllholi() {
 		List<HolidayModel> listHoli = holiSv.findAllHoliday();
@@ -56,7 +51,7 @@ public class NgayNghi {
 
 	@RequestMapping(value = "/holi/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<HolidayDTO> getholiById(@PathVariable("id") Integer id) {
-		var data = holiSv.findHolidayById(id).get();
+		var data = holiSv.findHolidayById(id);
 		//mapper từ entity -> DTO
 		HolidayDTO holi = ModelMapperConfig.modelMapper.map(data, HolidayDTO.class);
 
@@ -69,7 +64,7 @@ public class NgayNghi {
 	public ResponseEntity<HolidayDTO> createholi(@RequestBody HolidayDTO holi, UriComponentsBuilder builder) {
 		//mapper từ DTO -> entity
 		HolidayModel holiModel = ModelMapperConfig.modelMapper.map(holi, HolidayModel.class);
-		holiSv.save(holiModel);
+		//holiSv.save(holiModel);
 		holi.setId(holiModel.getId());
 		HttpHeaders headers = new HttpHeaders();
 		headers.setLocation(builder.path("/holi/{id}").buildAndExpand(holi.getId()).toUri());
@@ -80,7 +75,7 @@ public class NgayNghi {
 	public ResponseEntity<HolidayModel> updateholi(@PathVariable("id") Integer id, @RequestBody HolidayModel holi) {
 		Optional<HolidayModel> currentholi = holiSv.findHolidayById(id);
 
-		if (!currentholi.isPresent()) {
+		if (currentholi == null) {
 			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 		}
 
@@ -95,10 +90,10 @@ public class NgayNghi {
 	@RequestMapping(value = "/holi/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<HolidayModel> deleteholi(@PathVariable("id") Integer id) {
 		Optional<HolidayModel> holi = holiSv.findHolidayById(id);
-		if (!holi.isPresent()) {
+		if (holi == null) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
-		holiSv.remove(holi.get());
+		//holiSv.remove(holi.get());
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 }
